e123c91904a88094ce1da1f1d945c0430159d471
Yi Wang <y.wang7@uqconnect.edu.au>
2019-12-03 19:12:49 +1000

    Init Todo App with Typescript.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type TodoType = {
+    id: number, val: string
+}
+
+type TodoProps = {
+    todo: TodoType,
+    // function definition by arrow function
+    remove: (id: number) => void;
+}
+
+type TodoListProps = {
+    todos: TodoType[],
+    // function definition by arrow function
+    remove: (id: number) => void;
+}
+
+type TodoFormProps = {
+    addTodo: (val: string) => void;
+}
+
+const Todo = ({ todo, remove }: TodoProps) => {
+    return (
+        <li onClick={() => remove(todo.id)}>
+            {todo.val}
+        </li>
+    );
+};
+
+const TodoList = ({ todos, remove }: TodoListProps) => {
+    return (
+        <ul>
+            {todos.map(todo => {
+                return <Todo key={todo.id} todo={todo} remove={remove} />
+            })}
+        </ul>
+    )
+
+}
+
+const TodoForm = ({ addTodo }: TodoFormProps) => {
+    let input: HTMLInputElement;
+    return (
+        <div>
+            <!-- make sure we use correct type here -->
+            <input ref={(node: HTMLInputElement) => {
+                input = node;
+            }} />
+            <button onClick={() => {
+                addTodo(input.value);
+                input.value = "";
+            }}>
+                +
+	    </button>
+        </div >
+    )
+}
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todos, setTodos] = useState([
+        { id: 0, val: "a" },
+        { id: 1, val: "b" }
+    ]);
+
+    const [id, setId] = useState(2);
+    const handleAdd = (val: string) => {
+        let todo = { id: id, val: val };
+        todos.push(todo);
+        console.log(todos);
+        setTodos(todos);
+        setId(id + 1);
+    }
+    const handleRemove = (id: number) => {
+        let remainder: TodoType[] = [];
+        todos.map(todo => {
+            if (todo.id !== id) remainder.push(todo)
+        })
+        setTodos(remainder);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello world!</p>
+                <TodoForm addTodo={handleAdd} />
+                <TodoList todos={todos} remove={handleRemove} />
+            </header>
+        </div>
+    );
 }
 
 export default App;
